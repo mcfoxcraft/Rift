a44a80f04e1b9b62e3a280e966a7324b9d798adf
Noah van der Aa <ndvdaa@gmail.com>
2024-08-19 23:27:15 +0200

    Remove paperweight, we only need API

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java")
-    id("io.papermc.paperweight.userdev") version "1.7.2"
     id("xyz.jpenilla.run-paper") version "2.1.0"
 }
 
@@ -9,21 +8,16 @@ version = "2.0.7"
 val apiVersion = "1.21"
 
 repositories {
-    maven("https://maven.fabricmc.net/")
-    maven("https://dl.cloudsmith.io/public/arcane/archive/maven/")
+    maven("https://papermc.io/repo/repository/maven-public/")
     mavenCentral()
     mavenLocal()
 }
 
 dependencies {
-    paperweight.paperDevBundle("1.21.1-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
 }
 
 tasks {
-    assemble {
-        dependsOn(reobfJar)
-    }
-
     processResources {
         filesMatching("**/plugin.yml") {
             expand(
